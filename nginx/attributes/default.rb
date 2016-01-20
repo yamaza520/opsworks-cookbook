c2066ec2f4eab1bf82bae2a3c3578bc1d5e88d56
Atsushi Yamazaki <atsushi_yamazaki@relationsgroup.co.jp>
2016-01-20 16:06:15 +0900

    mod nginx

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -1,4 +1,4 @@
-default["nginx"]["play_proxy"][:hostname] = "example.jp"
+default["nginx"]["play_proxy"][:hostname] = "54.238.242.225"
 default["nginx"]["play_proxy"][:play_port] = 9000
 default["nginx"]["play_proxy"][:http_port] = 80
 default["nginx"]["play_proxy"][:https_port] = 8443
