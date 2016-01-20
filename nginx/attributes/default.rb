7f0e926329909857bd98a8c5a64a9537e00c8fdc
Atsushi Yamazaki <atsushi_yamazaki@relationsgroup.co.jp>
2016-01-20 11:54:06 +0900

    mod nginx

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -1,4 +1,4 @@
-default["nginx"]["node_proxy"][:hostname] = "example.jp"
-default["nginx"]["node_proxy"][:node_port] = 80
-default["nginx"]["node_proxy"][:http_port] = 8080
-default["nginx"]["node_proxy"][:https_port] = 8443
+default["nginx"]["play_proxy"][:hostname] = "example.jp"
+default["nginx"]["play_proxy"][:play_port] = 9000
+default["nginx"]["play_proxy"][:http_port] = 80
+default["nginx"]["play_proxy"][:https_port] = 8443
