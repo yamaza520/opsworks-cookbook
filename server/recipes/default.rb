753abf8df9e6598865948f3625dc6f41e390b83c
Atsushi Yamazaki <atsushi_yamazaki@relationsgroup.co.jp>
2016-01-26 11:25:31 +0900

    mod user

--- server/recipes/default.rb
+++ server/recipes/default.rb
@@ -3,11 +3,16 @@ user "rl_app" do
   supports :manage_home => true
   action :create
 end
+group "wheel" do
+  action [:modify]
+  members ["rl_app"]
+  append true
+end
 
 # create directory
 directory "/var/play" do
   owner "rl_app"
-  group "rl_app"
+  group "wheel"
   recursive true
   mode 0755
   action :create_if_missing
